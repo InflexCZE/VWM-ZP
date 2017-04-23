f5088cf8ac3ddca5018ec2eb61d68a5f7b4d44e8
InflexCZE <pavlim45@fit.cvut.cz>
2017-04-23 16:40:58 +0200

    Pearson-Spree

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import * as Koa from 'koa'
 import * as KoaLogger from 'koa-logger'
 import * as KoaSession from 'koa-session-minimal'
 import * as KoaRouter from 'koa-router'
-import { sequelize } from './models'
+import { sequelize, User, Rating, Movie } from './models'
+import * as Pearson from "./pearson"
 
 const app = new Koa()
 
@@ -12,8 +13,101 @@ app.use(KoaSession())
 
 const router = new KoaRouter()
 
+router.get('/Detail', async function (ctx, next) 
+{
+  const id1 = ctx.query.id1;
+  const id2 = ctx.query.id2;
+
+  const user1 = await User.findById(id1, 
+  {
+      include: 
+      [
+        {
+          model: Rating, as: 'ratings',
+          include: 
+          [
+            { model: Movie, as: 'movie' }
+          ]
+        }
+      ]
+    });
+
+  const user2 = await User.findById(id2, 
+  {
+      include: 
+      [
+        {
+          model: Rating, as: 'ratings',
+          include: 
+          [
+            { model: Movie, as: 'movie' }
+          ]
+        }
+      ]
+    });
+
+    const X = user1.ratings;
+    const Y = user2.ratings;
+
+    Pearson.MakeRatingsDeduplication(X, Y);
+    const data = Pearson.MakeChartData(X, Y);
+    const index = Pearson.ComputeCorrelationCoefficient(X, Y);
+    
+    // const myBubbleChart = new Chart(ctx,
+    // {
+    //     type: 'bubble',
+    //     data: data,
+    //     options: 
+    //     {
+    //         elements: 
+    //         {
+    //             points: 
+    //             {
+    //                 borderWidth: 1,
+    //                 borderColor: 'rgb(0, 0, 0)'
+    //             }
+    //         }
+    //     }
+    // });
+});
+
 router.get('/', async function (ctx, next) {
-  ctx.body = 'TODO'
+  ctx.body = 'TODO \n';
+
+  //  const user= await User.findById(666);
+   
+  //  User.create
+  //  ({
+  //   name: "hFHKDKSJFhjKSD",
+  //   password: ""
+  //  });
+
+  //  User.create
+  //  ({
+  //     name: "fdasfsafdds2",
+  //     password: ""
+  //  });
+
+  const all = await User.findAll();
+  for(let x of all)
+  {
+      const user = await User.findById(666, {
+    include: [
+      {
+        model: Rating, as: 'ratings',
+        include: [
+          { model: Movie, as: 'movie' }
+        ]
+      }
+    ]
+  });
+
+
+    Rating.find({where: {userId: x.id}})
+    
+
+    ctx.body += `${x.name}\n`;
+  }
 })
 
 app.use(router.routes())
---
+++ src/pearson.ts
@@ -0,0 +1,152 @@
+import { sequelize, User, Rating, Movie } from './models'
+import { Instance as UserInstance } from './models/def/user'
+import { Instance as RatingInstance } from './models/def/rating'
+
+export interface DataSet
+{
+    label: string
+    hoverColor:string
+    backgroundColor:string
+    data: {x:number, y:number, r:number}[]
+}
+
+export function MakeRatingsDeduplication(xRatings : RatingInstance[], yRatings : RatingInstance[])
+{
+    xRatings.sort((a, b) => b.movieId - a.movieId);
+    yRatings.sort((a, b) => b.movieId - a.movieId);
+
+    const toRemoveX : number[] = [];
+    const toRemoveY : number[] = [];
+
+    let i = 0, j = 0;
+    while(i < xRatings.length && j < yRatings.length)
+    {
+        let xRate = xRatings[i];
+        let yRate = yRatings[j];
+
+        let xMovie = xRate.movieId;
+        let yMovie = yRate.movieId;
+
+        if(xMovie == yMovie)
+        {
+            i++;
+            j++;
+        }
+        else if(xMovie < yMovie)
+            toRemoveX.push(i++);
+        else
+            toRemoveY.push(j++);
+    }
+
+    for(let i = toRemoveX.length - 1; i >= 0; i--)
+        xRatings.splice(toRemoveX[i], 1);
+
+    for(let i = toRemoveY.length - 1; i >= 0; i--)
+        yRatings.splice(toRemoveY[i], 1);
+
+    Assert(() => xRatings.length == yRatings.length);
+}
+
+export function ComputeCorrelationCoefficient(xRatings : RatingInstance[], yRatings : RatingInstance[])
+{
+    const ratingsX = xRatings.map(x => x.value);
+    const ratingsY = yRatings.map(x => x.value);
+
+    // xRatings.sort((a, b) => b.movieId - a.movieId);
+    // yRatings.sort((a, b) => b.movieId - a.movieId);
+
+    // let xIndex = 0, yIndex = 0;
+    // const ratingsX : number[] = [];
+    // const ratingsY : number[] = [];
+
+    // let i = 0, j = 0;
+    // while(i < xRatings.length && j < yRatings.length)
+    // {
+    //     let xRate = xRatings[i];
+    //     let yRate = yRatings[j];
+
+    //     let xMovie = xRate.movieId;
+    //     let yMovie = yRate.movieId;
+
+    //     if(xMovie == yMovie)
+    //     {
+    //         i++;
+    //         j++;
+    //         ratingsX.push(xRate.value);
+    //         ratingsY.push(yRate.value);
+    //     }
+    //     else if(xMovie < yMovie)
+    //         i++;
+    //     else
+    //         j++;
+    // }
+
+    Assert(() => ratingsX.length == ratingsY.length);
+
+    if(ratingsX.length == 0)
+        return -1;
+
+    const sampleMeanX = SampleMean(ratingsX);
+    const sampleMeanY = SampleMean(ratingsY);
+    const ssdX = SampleStandardDeviation(ratingsX, sampleMeanX);
+    const ssdY = SampleStandardDeviation(ratingsY, sampleMeanY);
+
+    let sum = 0;
+    for(let i = 0; i < ratingsX.length; i++)
+    {
+        let xPart = (ratingsX[i] - sampleMeanX) / ssdX;
+        let yPart = (ratingsY[i] - sampleMeanY) / ssdY;
+        sum += xPart * yPart;
+    }
+
+    return sum / (ratingsX.length - 1);
+}
+
+export function MakeChartData(xRatings : RatingInstance[], yRatings : RatingInstance[]) : DataSet[]
+{
+    var result = new Array<DataSet>(xRatings.length);
+
+    for(let i = 0; i < xRatings.length; i++)
+    {
+        const x = xRatings[i];
+        const y = xRatings[i];
+
+        result[i] = 
+        {
+            label: x.movie.name,
+            data: [ {x: x.value, y: y.value, r: 15} ],
+            hoverColor: '#0066FF',
+            backgroundColor: '#0066FF'
+        };
+    }
+
+    return result;
+}
+
+function SampleStandardDeviation(ratings : number[], sampleMean : number)
+{
+    let sum = 0;
+    for(let x of ratings)
+    {
+        let diff = x - sampleMean;
+        sum += diff * diff;
+    }
+
+    return Math.sqrt(sum / (ratings.length - 1));
+}
+
+function SampleMean(ratings : number[])
+{
+    let sum = 0;
+    for(var x of ratings)
+        sum += x;
+
+    return sum / ratings.length;
+}
+
+function Assert(fn: () => boolean)
+{
+  if (fn() === false)
+    throw new Error(fn.toString());
+  
+}
