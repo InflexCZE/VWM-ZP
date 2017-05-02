b12678f4df464d7ad72ba78b347169ca100e9ce3
Razem <info@razem.cz>
2017-05-02 11:53:51 +0200

    Relations

--- src/models/def/user.ts
+++ src/models/def/user.ts
@@ -1,11 +1,13 @@
 import * as X from '.'
 import * as Rating from './rating'
+import * as Rank from './rank'
 
 export interface Attributes extends X.Attributes {
   name?: string
   password?: string
 
   ratings?: Rating.Instance[]
+  ranks?: Rank.Instance[]
 }
 
 export interface Instance extends X.Instance<Attributes>, Attributes {}
