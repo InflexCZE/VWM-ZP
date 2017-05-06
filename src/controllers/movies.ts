c9760d4d8ea296fca9adf0f0dbeced5a1cdd8492
Razem <info@razem.cz>
2017-05-06 18:28:21 +0200

    Movie rating

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -60,13 +60,51 @@ router.get('/detail/:id', async function (ctx) {
     }
   })).get('avg')
 
+  let userRating
+  if (ctx.user) {
+    userRating = await Rating.findOne({
+      where: {
+        userId: ctx.user.id,
+        movieId: movie.id
+      }
+    })
+  }
+
   Object.assign(ctx.state, {
     movie,
     average,
-    ratings
+    ratings,
+    userRating
   })
 
   await ctx.render('movies/detail')
+})
+
+router.post('/rate/:id', isUser, async function (ctx) {
+  const id = +ctx.params.id
+  if (!id) return
+
+  const movie = await Movie.findById(id)
+  if (!movie) return
+
+  const value = parseInt(ctx.request.body.value, 10) || 0
+  if (value < 0 || value > 5) return
+
+  const [rating, created] = await Rating.findOrCreate({
+    where: {
+      userId: ctx.user.id,
+      movieId: movie.id
+    },
+    defaults: {
+      value
+    }
+  })
+
+  if (!created) {
+    await rating.update({ value })
+  }
+
+  ctx.redirect(`/movies/detail/${movie.id}`)
 })
 
 export default function (mainRouter: KoaRouter) {
